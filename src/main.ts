1f875c46d22f5b343c7ff447590dc7c9a1f8b644
Bangbiu <empty_hundred@outlook.com>
2025-06-18 11:57:01 -0400

    first deploy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { ThickGridHelper } from './ThickGridHelper';
+//import { ThickGridHelper } from './ThickGridHelper';
 
 // Scene
 const scene = new THREE.Scene();
---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/MultiblockBench/',  // 👈 IMPORTANT: use your GitHub repo name here
+});
